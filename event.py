a3bff59c974a7ad470b8b29d0bd287c0c8c23007
Sourabh-Singh-372 <76203601+Sourabh-Singh-372@users.noreply.github.com>
2024-05-28 11:55:16 +0530

    Update event.py

--- event.py
+++ event.py
@@ -9,11 +9,11 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-# mycursor.execute("use events")
-# # mycursor.execute("create table new(name char(20), profession varchar(50), age int(4))")
-# mycursor.execute("insert into new(name, profession, age) values ('Ravi', 'software engineer', 24), ('Akash', 'software engineer', 23), ('Bhanu', 'mechanical engineer', 22), ('Mukund', 'electrical engineer', 24)")
-# mycursor.execute("select name, email, mobile from new")
-# data = mycursor.fetchall()
+mycursor.execute("use events")
+mycursor.execute("create table new(name char(20), profession varchar(50), age int(4))")
+mycursor.execute("insert into new(name, profession, age) values ('Ravi', 'software engineer', 24), ('Akash', 'software engineer', 23), ('Bhanu', 'mechanical engineer', 22), ('Mukund', 'electrical engineer', 24)")
+mycursor.execute("select name, email, mobile from new")
+data = mycursor.fetchall()
 
-# for name, email, mobile in data:
-#     print(name, email, mobile)
+for name, email, mobile in data:
+    print(name, email, mobile)
